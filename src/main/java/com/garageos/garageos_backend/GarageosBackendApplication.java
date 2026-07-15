8a11d59ffa388d7cce8c9e6ae161d1578aad5ffc
Sri Varun Varma <varun2varma@gmail.com>
2026-07-15 22:43:58 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/garageos/garageos_backend/GarageosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.garageos.garageos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarageosBackendApplication.class, args);
+	}
+
+}
