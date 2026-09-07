6923bfa0da589c6c7aa5d95e196bd600cbb4cfcc
Sri Varun Varma <varun2varma@gmail.com>
2026-09-07 21:03:31 +0530

    Fix navigation coordinates migration

--- src/main/resources/db/migration/V30__add_navigation_coordinates.sql
+++ src/main/resources/db/migration/V30__add_navigation_coordinates.sql
@@ -1,4 +1,4 @@
-ALTER TABLE navigation_requests
+ALTER TABLE navigation_request
     ADD COLUMN pickup_latitude NUMERIC(10,7),
     ADD COLUMN pickup_longitude NUMERIC(10,7),
     ADD COLUMN delivery_latitude NUMERIC(10,7),
